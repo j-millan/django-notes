be4c5547c33314ef275209dacb414f72d07f6d43
Jhonattan Millán <millanjhonattan0@gmail.com>
2020-10-30 20:40:16 -0400

    Add home view and its tests

--- notes/views.py
+++ notes/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from notes.models import Note
 
-# Create your views here.
+def home(request):
+	notes = Note.objects.all()
+	return render(request, 'notes/home.html', {'notes': notes})
